3765c7d9c54b38b4d8c9c1828fc31b8c4cc4e06c
joel andrade <mariborgesdatascientist@gmail.com>
2026-02-07 01:06:38 -0300

    yy

--- infra/variables.tf
+++ infra/variables.tf
@@ -112,7 +112,7 @@ variable "gold_cluster_name" {
 variable "cluster_node_type_id" {
   description = "Node type for Databricks clusters (Azure: Standard_DS3_v2, AWS: i3.xlarge)"
   type        = string
-  default     = "Standard_DS3_v2"
+  default     = "i3.xlarge"
 }
 
 variable "cluster_spark_version" {
